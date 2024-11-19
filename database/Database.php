fadbe9c417348f0f84b4920d3632502ce8a86354
Bilal094 <bilalachrifi@gmail.com>
2024-11-19 12:14:27 +0100

    Changed database API (mysqli to pdo)

--- database/Database.php
+++ database/Database.php
@@ -2,45 +2,36 @@
 
 class Database
 {
-    protected string $servername;
-    protected string $username;
-    protected $conn;
-    protected $sql;
+    protected string $servername, $username;
+    protected $conn, $sql;
 
     static function connectDatabase(string $servername, string $username)
     {
-        $conn = new mysqli($servername, $username);
-        if ($conn->connect_error)
-        {
-            die("Connection to database failed: " . $conn->connect_error);
-        } else {
-            return $conn;
-        }
-    }
-
-    static function createDatabase(string $dbname, $conn)
-    {
-        $sql = "CREATE DATABASE IF NOT EXISTS libraryManager";
-        if ($conn->query($sql) === TRUE)
-        {
+        try {
+            $conn = new PDO("mysql:host=$servername", $username);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $sql = "CREATE DATABASE IF NOT EXISTS libraryManager";
+            $conn->exec($sql);
             Database::createUserTable($conn);
-        } else {
-            echo "Database creation failed: " . $conn->error;
-        };
+        } catch (PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+        }
     }
 
     static function createUserTable($conn)
     {
-        $database = $conn->select_db("libraryManager");
-        $sql = "CREATE TABLE IF NOT EXISTS users (
-                id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-                email VARCHAR(50) NOT NULL UNIQUE,
-                password VARCHAR(50) NOT NULL,
-                reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-                )";
-        if ($conn->error)
-        {
-            echo "User table creation failed: " . $conn->error;
-        } 
+        try {
+            $conn->query("USE libraryManager");
+            $sql = "CREATE TABLE IF NOT EXISTS users (
+            id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+            email VARCHAR(50) UNIQUE NOT NULL,
+            password VARCHAR(30) NOT NULL,
+            reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+            )";
+            $conn->exec($sql);
+            $conn = null;
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
     }
 }
